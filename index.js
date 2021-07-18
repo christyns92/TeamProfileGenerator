76fa6994f8cfaaa98fd3db2f5771fe8e19c82f00
Christyn Garcia <christynsandusky@gmail.com>
2021-07-18 15:51:03 -0500

    editing js files

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const allEmployees = []
 
 const fs = require('fs');
 const inquirer = require('inquirer');
-
 const Engineer = require('./lib/Engineer');
-const Intern = require('./lib.Intern');
+const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
 const generateHtml = require('./utils/generateHtml');
